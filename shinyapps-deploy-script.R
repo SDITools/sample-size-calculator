31c0d49caeab04d11a9778193d9cddd537f8204b
Tim Wilson <timwilson7@gmail.com>
2018-05-21 09:40:59 -0400

    Initial pass at "runtime-based" calculator added. Also added deploy script for that calculator.

--- shinyapps-deploy-script.R
+++ shinyapps-deploy-script.R
@@ -1,8 +1,16 @@
 # Use this script -- or some tweaked version of it -- to push the code
-# to shinyapps.io.
+# to shinyapps.io. Comment out the deployApp() call that you do NOT 
+# want to run. This script requires having rsconnect installed, as well
+# as having authenticated with a shinyapps.io 
 
 library(rsconnect)
 
+# Deploy MDE-based calculator
 deployApp(appFiles = c("sample-size-calc.Rmd","styles.css"),
           appName = "sample-size-calculator",
           appTitle = "Sample Size Calculator")
+
+
+deployApp(appFiles = c("sample-size-calc-runtime.Rmd","styles.css"),
+          appName = "sample-size-calculator-runtime",
+          appTitle = "Sample Size Calculator - Runtime-Based")
